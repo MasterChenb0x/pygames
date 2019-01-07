e68760dbd3cdaeb8fc0a80f7d48121a618db35c5
chenb0x <artofmorellc@gmail.com>
2019-01-06 19:28:21 -0800

    Code organization for rps.py

--- rps.py
+++ rps.py
@@ -1,8 +1,13 @@
 #!/usr/local/bin/python3
 
+#Import the Universe
 import random
 
+#Reusable code (functions)
 def selectionConvert(selection):
+	'''
+	Convert selection to an integer for Maths.
+	'''
 	if selection == "r":
 		return 0
 	elif selection == "p":
@@ -11,6 +16,9 @@ def selectionConvert(selection):
 		return 2
 
 def compare(playerHand, computerHand):
+	'''
+	Function to calculate the winner
+	'''
 	result = playerHand - computerHand
 	if result % 3 == 0:
 		return "TIE!"
@@ -19,13 +27,20 @@ def compare(playerHand, computerHand):
 	elif result % 3 == 2:
 		return "LOSE!" 
 
-userSelect = input("Make your selection: (r)ock, (p)aper, or (s)cissors: ")
-userSelect = userSelect.lower()
-print(userSelect)
-userSelect = selectionConvert(userSelect)
-print(userSelect)
-compSelect = random.randint(0,2)
-print(compSelect)
-outcome = compare(userSelect, compSelect)
-print(outcome)
+def main():
+	#Set defaults
+	play = "y"
 
+	while play == "y":
+		userSelect = input("Make your selection: (r)ock, (p)aper, or (s)cissors: ")
+		userSelect = userSelect.lower()
+		print(userSelect)
+		userSelect = selectionConvert(userSelect)
+		print(userSelect)
+		compSelect = random.randint(0,2)
+		print(compSelect)
+		outcome = compare(userSelect, compSelect)
+		print(outcome)
+		play = input("Would you like to play again? (y/N): ")
+
+main()
